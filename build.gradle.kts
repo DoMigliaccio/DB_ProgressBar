78469c16b4698f487421c0152274913a29ba8b0d
Do.Migliaccio <dome.migliaccio9@gmail.com>
2024-04-15 13:59:39 +0200

    Extended Compatibility Range

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.intellij") version "1.9.0"
 }
 
-group = "com.example"
+group = "com.DoMigliaccio"
 version = "3.1.3"
 
 repositories {
@@ -13,7 +13,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.1.4")
+    version.set("2024.1")
+    /*version.set("2022.1.4")*/
     type.set("IC") // Target IDE Platform
     plugins.set(listOf(/* Plugin Dependencies */))
 }
@@ -28,7 +29,7 @@ tasks {
     patchPluginXml {
         //sinceBuild.set("221")
         sinceBuild.set("211")
-        untilBuild.set("241.*")
+        /*untilBuild.set("241.*") - Not specifying until-build means it will include all future builds. This includes future, yet unreleased versions and possibly new IDEs, which might impact compatibility later.*/
     }
 
     signPlugin {
